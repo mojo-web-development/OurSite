617d793dab08831e2030bc57f4b95bb83b92241c
dallindooks <dallinmichaelwright@gmail.com>
2016-07-12 12:51:34 -0600

    I put the background on the home page

--- main.js
+++ main.js
@@ -2,13 +2,24 @@ $(document).ready(function(){
   function scroll (){
     var top = document.getElementsByClassName('navbar');
     var ypos = window.pageYOffset;
-    if(ypos < 0){
-      top.style.height = "transparent";
+    if(ypos > 25){
+      top.style.height = "#07a8c5";
     }
         else {
-          top.style.background = "#07a8c5";
+          top.style.background = "transparent";
               };
             };
   window.addEventListener("scroll",scroll);
 
 });
+
+(function(){
+
+    var documentEl = $(document),
+       parallaxBg = $('.container');
+
+        documentEl.on('scroll', function() {
+          var currScrollPos = documentEl.scrollTop();
+          parallaxBg.css('background-position', '0 ', + -currScrollPos/2 + 'px');
+      });
+    });
