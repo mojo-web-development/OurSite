7e8d15619fdea4e4cc9ba75fc508e4dafedf7e66
dallindooks <dallinmichaelwright@gmail.com>
2016-07-12 14:37:53 -0600

    I put content boxes and added a parallax effect

--- main.js
+++ main.js
@@ -1,25 +1,10 @@
-$(document).ready(function(){
-  function scroll (){
-    var top = document.getElementsByClassName('navbar');
-    var ypos = window.pageYOffset;
-    if(ypos > 25){
-      top.style.height = "#07a8c5";
-    }
-        else {
-          top.style.background = "transparent";
-              };
-            };
-  window.addEventListener("scroll",scroll);
+var container
+var ypos
+function parralax (){
 
-});
+  ypos = window.pageYOffset;
+  container = document.getElementById('container');
 
-(function(){
-
-    var documentEl = $(document),
-       parallaxBg = $('.container');
-
-        documentEl.on('scroll', function() {
-          var currScrollPos = documentEl.scrollTop();
-          parallaxBg.css('background-position', '0 ', + -currScrollPos/2 + 'px');
-      });
+  container.style.top  = ypos * .4 + 'px';
     });
+    window.addEventListener("scroll",parralax);
